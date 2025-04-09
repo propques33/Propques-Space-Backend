53734832d77523c7975efe41b4aff8a385da5ae5
Harsh D. Rao <harshrao26>
2025-04-09 14:40:32 +0530

    Code Changed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 connectDB();
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: process.env.CORS_ORIGIN || "*" }));
 app.use(express.json());
 
 app.use("/api/locations", locationRoutes);
